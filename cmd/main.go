2ae9a0f01e3862860e4ba76e846c4ed2d179cefc
Tolebi <tolebizaksybaj@gmail.com>
2024-06-29 21:46:49 +0500

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/spf13/viper"
+	"log"
+	"todo"
+	"todo/pkg/handler"
+	"todo/pkg/repository"
+	"todo/pkg/service"
+)
+
+func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs:  %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
